529aff4dece86612c2e975c00b623b4245897dab
bchronowski98 <bchronowski98@gmail.com>
2022-11-07 11:30:29 +0100

    chyba git

--- src/components/GetWeather.jsx
+++ src/components/GetWeather.jsx
@@ -1,30 +1,16 @@
 import React, { useState, useEffect } from "react";
 import styles from "./GetWeather.module.scss";
-import Sunny from "../assets/day-sunny.svg";
-import Rain from "../assets/rain.svg";
+import { ReactComponent as Sunny } from "../assets/day-sunny.svg";
+import { ReactComponent as Rain } from "../assets/rain.svg";
+import { ReactComponent as XSign } from "../assets/x-sign.svg";
+import useFetch from "../hooks/useFetch.js";
 
-const GetWeather = () => {
-  const url = "https://live-weather.deno.dev/city?Cracow";
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+const url = "https://live-weather.deno.dev";
+const cracowWeather = "/city?Cracow";
+const rainTreshold = 0.7;
 
-  useEffect(() => {
-    const loadData = async () => {
-      try {
-        const res = await fetch(url);
-        const data = await res.json();
-        setData(data);
-        setLoading(false);
-        console.log(data);
-      } catch (e) {
-        setError(e);
-        setLoading(false);
-      }
-    };
-
-    loadData();
-  }, []);
+const GetWeather = ({ toggle, toggleWeather }) => {
+  const { data, loading, error } = useFetch(url + cracowWeather);
 
   if (loading) {
     return <p>loading...</p>;
@@ -35,16 +21,19 @@ const GetWeather = () => {
   }
 
   return (
-    <div className={styles.main}>
-      <div className={styles.info}>
-        <h4>Krakow</h4>
-        <h4>{Math.round(data.temperature)}</h4>
-      </div>
-      <div className={styles.icons}>
-        {data.precipitation > 0.7 ? (
-          <img src={Rain} alt="rain" />
-        ) : (
-          <img src={Sunny} alt="sunny" />
+    <div className={`${styles.main} ${toggle ? "" : styles.toggled}`}>
+      <div className={styles.card}>
+        <div className={styles.info}>
+          <h4>Krakow</h4>
+          <h4>{Math.round(data.temperature * 10) / 10}&deg;</h4>
+        </div>
+        <div className={styles.icons}>
+          {data.precipitation > rainTreshold ? <Sunny /> : <Rain />}
+        </div>
+        {toggle && (
+          <button onClick={toggleWeather}>
+            <XSign />
+          </button>
         )}
       </div>
     </div>
