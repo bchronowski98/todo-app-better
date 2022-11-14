caee80767e7e9170bce65dc80cbe079df6a3bff5
bchronowski98 <bchronowski98@gmail.com>
2022-11-14 19:23:25 +0100

    cities + checkbox

--- src/components/GetWeather.jsx
+++ src/components/GetWeather.jsx
@@ -1,42 +1,139 @@
-import React, { useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./GetWeather.module.scss";
 import { ReactComponent as Sunny } from "../assets/day-sunny.svg";
 import { ReactComponent as Rain } from "../assets/rain.svg";
-import { ReactComponent as XSign } from "../assets/x-sign.svg";
 import useFetch from "../hooks/useFetch.js";
 import useToggleOnOutsideClick from "../hooks/useToggleOnOutsideClick.jsx";
 
 const url = "https://live-weather.deno.dev";
-const cracowWeather = "/city?Cracow";
+const getCities = "/get-cities";
 const rainTreshold = 0.7;
 
 const GetWeather = ({ toggle, setToggle, toggleWeather }) => {
-  const { data, loading, error } = useFetch(url + cracowWeather);
+  const [city, setCity] = useState("Cracow");
+  const [checkbox, setCheckbox] = useState(false);
   const ref = useRef();
   useToggleOnOutsideClick(ref, toggle, setToggle);
+  const interval = useRef(null);
+  const cityUrl = `/city?${city}`;
+  const {
+    data: weatherData,
+    loading: loadingWeather,
+    error: errorWeather,
+  } = useFetch(url + cityUrl);
+  const {
+    data: cityData,
+    loading: loadingCity,
+    error: errorCity,
+  } = useFetch(url + getCities);
+  const [loading, setLoading] = useState(loadingWeather);
+  const [error, setError] = useState(errorWeather);
+  const [generalWeather, setGeneralWeather] = useState(null);
 
-  if (loading) {
+  // console.log(cities);
+
+  useEffect(() => {
+    if (checkbox) {
+      interval.current = setInterval(() => {
+        const loadData = async () => {
+          try {
+            const res = await fetch(url + cityUrl);
+            const data = await res.json();
+            setGeneralWeather(data);
+            setLoading(false);
+            console.log(data);
+          } catch (e) {
+            setError(e);
+            setLoading(false);
+          }
+        };
+        loadData();
+      }, 30000);
+    } else {
+      clearInterval(interval.current);
+      interval.current = null;
+    }
+    return () => clearInterval(interval.current);
+  }, [checkbox, city]);
+
+  useEffect(() => {
+    setGeneralWeather(null);
+  }, [city]);
+
+  const handleSubmitCity = (e) => {
+    e.preventDefault();
+    setCity(e.target.value);
+  };
+
+  const handleSubmitCheckbox = () => {
+    console.log("checkbox", !checkbox);
+  };
+
+  if (loadingWeather) {
     return <p>loading...</p>;
   }
 
-  if (error) {
+  if (errorWeather) {
     return <p>error</p>;
   }
 
   return (
     <div ref={ref} className={`${styles.main} ${toggle ? "" : styles.toggled}`}>
-      <div className={styles.card}>
-        <div className={styles.info}>
-          <h4>Krakow</h4>
-          <h4>{Number.parseFloat(data.temperature).toFixed(1)}&deg;</h4>
+      <div className={styles.contentContainer}>
+        <div className={styles.card}>
+          <div className={styles.info}>
+            <h4>{city}</h4>
+            <h4>
+              {Number.parseFloat(
+                generalWeather
+                  ? generalWeather.temperature
+                  : weatherData.temperature
+              ).toFixed(1)}
+              &deg;
+            </h4>
+          </div>
+          <div className={styles.icons}>
+            {generalWeather ? (
+              generalWeather.precipitation < rainTreshold ? (
+                <Sunny />
+              ) : (
+                <Rain />
+              )
+            ) : weatherData.precipitation < rainTreshold ? (
+              <Sunny />
+            ) : (
+              <Rain />
+            )}
+          </div>
+          {/*{toggle && (*/}
+          {/*  <button type="button" onClick={toggleWeather}>*/}
+          {/*    <XSign />*/}
+          {/*  </button>*/}
+          {/*)}*/}
         </div>
-        <div className={styles.icons}>
-          {data.precipitation > rainTreshold ? <Sunny /> : <Rain />}
-        </div>
-        {toggle && (
-          <button type="button" onClick={toggleWeather}>
-            <XSign />
-          </button>
+        {!loadingCity && !errorWeather && (
+          <div className={styles.extraFeatures}>
+            <form onChange={handleSubmitCity}>
+              <select name="cities">
+                <option value="" hidden={true}>
+                  Cracow
+                </option>
+                {cityData.map((city) => (
+                  <option key={city} value={city}>
+                    {city}
+                  </option>
+                ))}
+              </select>
+            </form>
+            <form onChange={handleSubmitCheckbox}>
+              <input
+                type="checkbox"
+                id="check"
+                onChange={() => setCheckbox((prevState) => !prevState)}
+              />
+              <label htmlFor="check">Refresh weather every 30s</label>
+            </form>
+          </div>
         )}
       </div>
     </div>
